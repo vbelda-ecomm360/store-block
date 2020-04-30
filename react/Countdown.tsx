7554deb1174d5c796f1c2b23aa4bd0478fdc1576
vbelda-ecomm360 <vbelda@ecomm360.es>
2020-04-30 15:57:01 +0200

    Stage 7

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -4,18 +4,14 @@ import { tick } from './utils/time'
 
 import { useCssHandles } from "vtex.css-handles"
 
-import { FormattedMessage } from 'react-intl'
-
 interface CountdownProps {
-  title: string,
   targetDate: string 
 }
 const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
 
-const CSS_HANDLES = ["container", "countdown", "title"]
+const CSS_HANDLES = ['countdown']
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  title,
   targetDate = DEFAULT_TARGET_DATE 
 }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
@@ -26,14 +22,11 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
 
   const handles = useCssHandles(CSS_HANDLES)
 
-  const titleText = title || <FormattedMessage id="countdown.title" />
-
   tick(targetDate, setTime)
 
   return (
     <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} db tc`}>{titleText}</div>
-      <div className={`${handles.countdown} db tc`}>
+      <div className={`db tc`}>
         {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
       </div>
     </div>
@@ -45,11 +38,6 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    title: {
-         title: 'I am a title',
-         type: 'string',
-         default: null,
-    },
     targetDate: {
         title: 'Final date',
         description: 'Final date used in the countdown',
