ff326fb66c6d9e041423c88faeadecb8661adcde
vbelda-ecomm360 <vbelda@ecomm360.es>
2020-04-30 16:45:45 +0200

    Stage 8

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,28 +1,49 @@
 import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
-
+import { useQuery } from 'react-apollo'
+import useProduct from 'vtex.product-context/useProduct' 
 import { useCssHandles } from "vtex.css-handles"
+import productReleaseDateQuery from './queries/productReleaseDate.graphql'
 
 interface CountdownProps {
-  targetDate: string 
 }
-const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
 
+const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString()
 const CSS_HANDLES = ['countdown']
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
-  targetDate = DEFAULT_TARGET_DATE 
-}) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
       hours: '00',
       minutes: '00',
       seconds: '00'
   })
-
   const handles = useCssHandles(CSS_HANDLES)
+  
+  const { product: { linkText } } = useProduct()
+  const { data, loading, error } = useQuery(productReleaseDateQuery, {
+    variables: {
+      slug: linkText
+    },
+    ssr: false
+  })
 
-  tick(targetDate, setTime)
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    ) 
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    )
+  }
 
   return (
     <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
@@ -33,17 +54,14 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   )
 }
 
+
+
+
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    targetDate: {
-        title: 'Final date',
-        description: 'Final date used in the countdown',
-        type: 'string',
-        default: null,
-     }
   },
 }
 
